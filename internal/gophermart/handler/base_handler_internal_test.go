8c81b6c265fd4d83fec8b49eda6e6db5204becf8
kozhevin <kozhevin.dima@gmail.com>
2023-07-21 09:38:46 +0300

    tests

--- internal/gophermart/handler/base_handler_internal_test.go
+++ internal/gophermart/handler/base_handler_internal_test.go
@@ -2,6 +2,7 @@ package handler
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -10,6 +11,7 @@ import (
 	"github.com/DimaKoz/go-musthave-diploma-impl/internal/gophermart/sqldb"
 	"github.com/labstack/echo/v4"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestAddAuthHeaders(t *testing.T) {
@@ -102,4 +104,96 @@ func TestWrapHandlerErr(t *testing.T) {
 			}
 		})
 	}
+}
+
+type testresponsewriter struct {
+	w http.ResponseWriter
+	// buf  bytes.Buffer
+	code int
+}
+
+func (rw *testresponsewriter) Header() http.Header {
+	return rw.w.Header()
+}
+
+func (rw *testresponsewriter) WriteHeader(statusCode int) {
+	rw.code = statusCode
+}
+
+func (rw *testresponsewriter) Write( /*data*/ _ []byte) (int, error) {
+	return 0, io.EOF
+
+	/*return rw.buf.Write(data)*/
+}
+
+func (rw *testresponsewriter) Done() (int64, error) {
+	return 0, io.EOF
+
+	/*	if rw.code > 0 {
+			rw.w.WriteHeader(rw.code)
+		}
+		return io.Copy(rw.w, &rw.buf)
+	*/
+}
+
+func TestWrapHandlerErrIO(t *testing.T) {
+	echoFr := echo.New()
+	req := httptest.NewRequest(echo.POST, "http://localhost:1323/admin/user_points/settings", nil)
+	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+	rec := httptest.NewRecorder()
+	ctx := echoFr.NewContext(req, rec)
+	ctx.Response().Writer = &testresponsewriter{w: rec} //nolint:exhaustruct
+
+	type args struct {
+		ctx        echo.Context
+		statusCode int
+		msg        string
+		errIn      error
+	}
+
+	arguments := args{
+		statusCode: http.StatusInternalServerError,
+		ctx:        ctx,
+		msg:        "error",
+		errIn:      errIn1,
+	}
+
+	err := WrapHandlerErr(arguments.ctx, arguments.statusCode, arguments.msg, arguments.errIn)
+	assert.Error(t, err)
+	assert.ErrorIs(t, err, io.EOF)
+}
+
+func TestGetAuthFromCtxEmpty(t *testing.T) {
+	echoFr := echo.New()
+	defer func(echoFramework *echo.Echo) {
+		err := echoFramework.Close()
+		require.NoError(t, err)
+	}(echoFr)
+
+	req := httptest.NewRequest(echo.GET, "http://localhost:1323", nil)
+	rec := httptest.NewRecorder()
+	ctx := echoFr.NewContext(req, rec)
+	got := GetAuthFromCtx(ctx)
+
+	assert.Empty(t, got)
+
+	ctx.Request().Header.Add("Authorization", "123")
+
+	got = GetAuthFromCtx(ctx)
+	assert.Empty(t, got)
+}
+
+func TestIsAuthorizedFalse(t *testing.T) {
+	echoFr := echo.New()
+	defer func(echoFramework *echo.Echo) {
+		err := echoFramework.Close()
+		require.NoError(t, err)
+	}(echoFr)
+
+	req := httptest.NewRequest(echo.GET, "http://localhost:1323", nil)
+	rec := httptest.NewRecorder()
+	ctx := echoFr.NewContext(req, rec)
+
+	got := IsAuthorized(ctx, nil)
+	assert.False(t, got)
 }
